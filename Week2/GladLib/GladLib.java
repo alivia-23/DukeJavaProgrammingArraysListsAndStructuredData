1e138a882d50c5aa4de1e9625f081f447cf4a9d9
alivia-23 <guin.al@husky.neu.edu>
2023-08-07 20:41:00 -0700

    code refactor

--- Week2/GladLib/GladLib.java
+++ Week2/GladLib/GladLib.java
@@ -1,6 +1,6 @@
 import edu.duke.*;
 import java.util.*;
-
+import java.io.*;
 public class GladLib {
     private ArrayList<String> adjectiveList;
     private ArrayList<String> nounList;
@@ -12,8 +12,7 @@ public class GladLib {
     private ArrayList<String> verbList;
     private ArrayList<String> fruitList;
     private ArrayList<String> usedList;
-    private int countRepeatWords = 0;
-    
+    private int countRep;
     private Random myRandom;
     
     private static String dataSourceURL = "http://dukelearntoprogram.com/course3/data";
@@ -22,23 +21,26 @@ public class GladLib {
     public GladLib(){
         initializeFromSource(dataSourceDirectory);
         myRandom = new Random();
+        usedList = new ArrayList<String>();
     }
     
     public GladLib(String source){
         initializeFromSource(source);
+        countRep = 0;
         myRandom = new Random();
     }
     
     private void initializeFromSource(String source) {
-        adjectiveList= readIt(source+"/adjective.txt");    
+        adjectiveList= readIt(source+"/adjective.txt"); 
         nounList = readIt(source+"/noun.txt");
         colorList = readIt(source+"/color.txt");
         countryList = readIt(source+"/country.txt");
-        nameList = readIt(source+"/name.txt");        
+        nameList = readIt(source+"/name.txt");      
         animalList = readIt(source+"/animal.txt");
-        timeList = readIt(source+"/timeframe.txt");
+        timeList = readIt(source+"/timeframe.txt"); 
         verbList = readIt(source+"/verb.txt");
         fruitList = readIt(source+"/fruit.txt");
+        
     }
     
     private String randomFrom(ArrayList<String> source){
@@ -68,34 +70,54 @@ public class GladLib {
         if (label.equals("timeframe")){
             return randomFrom(timeList);
         }
-        if (label.equals("verb")){
-            return randomFrom(verbList);
-        }
-        if (label.equals("fruit")){
-            return randomFrom(fruitList);
-        }
         if (label.equals("number")){
             return ""+myRandom.nextInt(50)+5;
+        }
+        if (label.equals("verb"))
+        {
+            return ""+randomFrom(verbList);
+        }
+        if (label.equals("fruit"))
+        {
+            return ""+randomFrom(fruitList);
         }
         return "**UNKNOWN**";
     }
     
     private String processWord(String w){
         int first = w.indexOf("<");
-        int last = w.indexOf(">",first);
+        int last = w.indexOf(">",first+1);
         if (first == -1 || last == -1){
             return w;
         }
+        //System.out.println("To be replaced: "+w);
         String prefix = w.substring(0,first);
         String suffix = w.substring(last+1);
         String sub = getSubstitute(w.substring(first+1,last));
-        if (!usedList.isEmpty()) {
-            return "Repeated";
-        } else {
+        //System.out.println(w.substring(first+1,last));
+        
+        if (usedList.isEmpty())
+        {
             usedList.add(sub);
-            countRepeatWords++;
+            //System.out.println(w+":"+sub);
+            //System.out.println(" ");
+            countRep++;
             return prefix+sub+suffix;
         }
+        if (usedList.contains(sub))
+        {
+            //System.out.println(w+":"+sub);
+            //System.out.println("Repeated");
+            return "repeat";
+        }
+        else
+        {
+            usedList.add(sub);
+            //System.out.println(w+":"+sub);
+            //System.out.println(" ");
+            countRep++;
+            return prefix+sub+suffix;
+        }  
     }
     
     private void printOut(String s, int lineWidth){
@@ -108,6 +130,8 @@ public class GladLib {
             System.out.print(w+" ");
             charsWritten += w.length() + 1;
         }
+        System.out.println(" ");
+        System.out.println("Total number of words replaced: "+countRep);
     }
     
     private String fromTemplate(String source){
@@ -115,15 +139,35 @@ public class GladLib {
         if (source.startsWith("http")) {
             URLResource resource = new URLResource(source);
             for(String word : resource.words()){
+                //System.out.println(word);
                 story = story + processWord(word) + " ";
             }
         }
         else {
             FileResource resource = new FileResource(source);
             for(String word : resource.words()){
-                story = story + processWord(word) + " ";
+                //System.out.println(word);
+                String sub = processWord(word);
+                int rep = 1;
+                while(rep == 1)
+                {
+                    
+                    if (sub.equals("repeat") == false)
+                    {
+                        rep=0;
+                        story = story + sub + " ";
+                    }
+                    else
+                    {
+                       sub=processWord(word); 
+                    }
+                }
+                
+                //System.out.println(story);
+                //System.out.println(" ");
             }
         }
+        System.out.println(" ");
         return story;
     }
     
@@ -145,14 +189,11 @@ public class GladLib {
     }
     
     public void makeStory(){
-        usedList.clear();
         System.out.println("\n");
         String story = fromTemplate("data/madtemplate2.txt");
         printOut(story, 60);
-        countRepeatWords = 0;
-        
+        countRep=0;
+        usedList.clear();
     }
-    
-
 
 }
